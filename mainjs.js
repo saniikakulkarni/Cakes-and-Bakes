390c45bda36c21d76b8f2fc0c5659e9afa0c783f
saniikakulkarni <64094450+saniikakulkarni@users.noreply.github.com>
2020-08-27 00:53:42 +0530

    New

---
+++ mainjs.js
@@ -0,0 +1,26 @@
+function popup()
+{
+    document.getElementById('popupid2').style.visibility="hidden";
+    document.getElementById('popupid').style.visibility="visible";
+}
+function popupclose()
+{
+    document.getElementById('popupid').style.visibility="hidden";
+}
+function popupsignup()
+{
+    document.getElementById('popupid').style.visibility="hidden";
+    document.getElementById('popupid2').style.visibility="visible";
+}
+function popupsignupclose()
+{
+    document.getElementById('popupid2').style.visibility="hidden";
+}
+function menushow()
+{
+    document.getElementById('dropdownmenu').style.visibility="visible";
+}
+function menuhide()
+{
+    document.getElementById('dropdownmenu').style.visibility="hidden";
+}
