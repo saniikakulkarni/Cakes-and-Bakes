dc396daac02ba7720075101499f823b6b7acb0cc
saniikakulkarni <64094450+saniikakulkarni@users.noreply.github.com>
2020-10-06 07:49:12 +0530

    php files

---
+++ includes/signininc.php
@@ -0,0 +1,54 @@
+<?php
+if(isset($_POST['signin-btn'])){
+    require "dbhinc.php";
+    $email = $_POST['email'];
+    $password = $_POST['pwd'];
+
+    $sql = "SELECT * FROM userdetails WHERE email=?";
+    $stmt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt,$sql))
+    {
+        header("Location: ../homepage.php?error=sqlerror");
+        exit();
+    }
+    else
+    {
+        mysqli_stmt_bind_param($stmt,"s",$email);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        if($row = mysqli_fetch_assoc($result))
+        {
+            $pwdCheck = password_verify($password,$row['password']);
+            if($pwdCheck == false)
+            {
+                header("Location: ../homepage.php?error=wrongpwd");
+                exit();
+            }
+            else if($pwdCheck == true)
+            {
+                session_start();
+                $_SESSION['userid'] = $row['userid'];
+                $_SESSION['email'] = $row['email'];
+                
+                header("Location: ../homepage.php?login=success");
+                exit();
+
+            }
+            else
+            {
+                header("Location: ../homepage.php?error=wrongpwd");
+                exit();
+            }
+        }
+        else
+        {
+            header("Location: ../homepage.php?error=nouser");
+            exit();
+        }
+    }
+}   
+else{
+    header("Location:../homepage.php");
+    exit();
+ }
+?>
