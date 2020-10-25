e0fbd072e0338713073b1a2aab6b7989a799479f
TejasV58 <64249206+TejasV58@users.noreply.github.com>
2020-10-25 16:09:37 +0530

    review part completed

--- includes/addreviewinc.php
+++ includes/addreviewinc.php
@@ -20,10 +20,42 @@
         else
         {
             mysqli_stmt_bind_param($stmt,"ssss",$userid,$itemid,$rating,$review);
-            mysqli_stmt_execute($stmt);
-            $_SESSION['success-message'] = "Review added successfully!";
-            header("Location:../templates/itempage.php?itemname=$itemname&success=review&added");
-            exit();
+            mysqli_stmt_execute($stmt); 
+            $sql1="SELECT AVG(rating) as avg_ratings,COUNT(*) as total_ratings FROM review GROUP BY itemid HAVING itemid=?";
+            $stmt1 = mysqli_stmt_init($conn);
+            if(!mysqli_stmt_prepare($stmt1,$sql1))
+            {
+                $_SESSION['error-message'] = 'Error!';
+                header("Location: ../templates/itempage.php?itemname=$itemname&error=sqlerror");
+                exit();
+            }
+            else
+            {
+                mysqli_stmt_bind_param($stmt1,"s",$itemid);
+                mysqli_stmt_execute($stmt1);
+                $result1 = mysqli_stmt_get_result($stmt1);
+                if($row1 = mysqli_fetch_assoc($result1))
+                {
+                   $avgratings = $row1['avg_ratings'];
+                   $totalratings = $row1['total_ratings'];
+                   $sql2="UPDATE item SET rating=?,star=? WHERE itemid=?";
+                   $stmt2=mysqli_stmt_init($conn);
+                   if(!mysqli_stmt_prepare($stmt2,$sql2))
+                   {
+                       $_SESSION['error-message'] = "Error!";
+                       header("Location:../templates/itempage.php?itemname=$itemname&error=sqlerror");
+                       exit();
+                   }
+                   else
+                   {
+                       mysqli_stmt_bind_param($stmt2,"sss",$totalratings,$avgratings,$itemid);
+                       mysqli_stmt_execute($stmt2);
+                       $_SESSION['success-message'] = "Review added successfully!";
+                        header("Location:../templates/itempage.php?itemname=$itemname&success=review&added");
+                        exit();
+                   }                          
+                }
+            } 
         }
         mysqli_stmt_close($stmt);
         mysqli_close($conn);
