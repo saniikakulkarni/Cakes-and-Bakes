f958bd8824e40d4fe7a674d1b1833ff0b46247d6
saniikakulkarni <64094450+saniikakulkarni@users.noreply.github.com>
2020-10-25 16:05:41 +0530

    track order display and cancel order

---
+++ includes/deleteorderitem.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+if(isset($_GET['orderid'])){
+    require "dbhinc.php";
+    $orderid = $_GET['orderid'];
+    $sql="DELETE FROM orderdetails WHERE orderid=? ";
+    $stmt=mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt,$sql))
+    {
+        $_SESSION['error-message'] = "Error!";
+        header("Location: ../templates/cart.php?error=sqlerror");                   
+        exit();
+    }
+    else
+    {
+        mysqli_stmt_bind_param($stmt,"s",$orderid);
+        mysqli_stmt_execute($stmt);      
+        $_SESSION['success-message']="Item removed from you order successfully!";
+        header("Location: ../templates/track-order.php");
+        exit();
+    }
+}
+else{
+    $_SESSION['error-message'] = "Error!";
+    header("Location:../templates/track-order.php");
+    exit();
+ }
+?>
--- templates/track-order.php
+++ templates/track-order.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cakes and Bakes | Results</title>
+    <title>Cakes and Bakes | Your Orders</title>
     <link rel="shortcut icon" type="image/png" href="Images/logo.png">
     <!--icons-->
     <script src="https://kit.fontawesome.com/a77f5500d1.js" crossorigin="anonymous"></script>
@@ -20,25 +20,14 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
     <!--Javascript-->
     <script src="mainjs.js"></script>
+    <style>
+        .inline-data
+        {
+            margin:2% 35%;
+        }
+    </style>
 </head>
 <body>
-    <?php
-        if(isset($_SESSION['email']))
-        {
-            if($_SESSION['email']=='admin@gmail.com')
-            require "headeradmin.php";
-            else
-            require "header.php";
-        }
-        else
-        require "header.php";
-        require "../includes/dbhinc.php";
-    ?>
-
-    <!-- Orders start -->
-    <h4 class="current-page">Profile > <span class="active">Your Orders</span></h4>
-    <h1 class="order-heading">Your Orders</h1>
-    <div class="orders">
     <?php
         ob_start();
         if(isset($_SESSION['email']))
@@ -52,20 +41,20 @@
         require "header.php";
         require "../includes/dbhinc.php";
     ?>
-    <!-- Cart start -->
-    <h4 class="current-page">Profile > <span class="active">Your Cart</span></h4>
-    <h1 class="order-heading"><img src="images/cart-icon.png" alt="" class=carticon>Your Cart </h1>
+    <!-- Orders start -->
+    <h4 class="current-page">Profile > <span class="active">Your Orders</span></h4>
+    <h1 class="order-heading">Your Orders</h1>
     <div class="orders">
         <?php
             if(isset($_SESSION['email']))
             {
                 if($_SESSION['email']!='admin@gmail.com')
                 {
-                    $sql="SELECT r.adddress,r.name,r.contactnumber,price,quantity,status,paymentmode,orderdate,deliverydate FROM orderdetails o JOIN  recipientdetails r  ON o.recipientid=r.recipientid  where userid=?";
+                    $sql="SELECT * FROM orderdetails o JOIN  recipientdetails r  ON o.recipientid=r.recipientid WHERE o.userid=?";
                     $stmt= mysqli_stmt_init($conn);
                     if(!mysqli_stmt_prepare($stmt,$sql))
                     {
-                        header("Location:./homepage.php?error=sqlerror");
+                        header("Location:./homepage.php?error=sqlerror1");
                         exit();
                     }
                     else
@@ -77,95 +66,94 @@
                         while($row = mysqli_fetch_assoc($result))
                         {
                             $flag=1;
-                            $stmt= mysqli_stmt_init($conn);
                             $sql2="SELECT name,img1 from item where itemid=?";
                             $stmt2= mysqli_stmt_init($conn);
                             if(!mysqli_stmt_prepare($stmt2,$sql2))
                             {
-                                header("Location:./homepage.php?error=sqlerror");
+                                header("Location:./homepage.php?error=sqlerror2");
                                 exit();
                             }
                             else
                             {
                                 mysqli_stmt_bind_param($stmt2,"i",$row['itemid']);
                                 mysqli_stmt_execute($stmt2);
-                                if(!mysqli_stmt_prepare($stmt2,$sql2))
+                                $result2= mysqli_stmt_get_result($stmt2);
+                                if($row2 = mysqli_fetch_assoc($result2))
                                 {
-                                    header("Location:./homepage.php?error=sqlerror");
-                                    exit();
-                                }
-                                else
-                                {
-                                    mysqli_stmt_bind_param($stmt2,"i",$row['itemid']);
-                                    mysqli_stmt_execute($stmt2);
-                                    $result2= mysqli_stmt_get_result($stmt2);
-                                    if($row2 = mysqli_fetch_assoc($result2))
-                                    {
-                                        $quantity=$row['quantity'];
-                                        $price=$row['price'];
-                                        $itemname=$row2['name'];
-                                        $img1=$row2['img1'];
-                                        $address=$row['address'];
-                                        $contactnumber=$row['contatnumber'];
-                                        $name=$row['name'];
-                                        echo "<div class='order'>
-                                                <div class='orderimgdiv'>
-                                                    <img src='./itemimages/$img1'  class='orderimg'>
-                                                </div>
-                                                <div class='orderdesc'>
-                                                    <h2 class='ordername'>$itemname</h2>
-                                                    <p class='cost'>$price</p>
-                                                    <p class='quantity'><span class='label'>Quantity :</span>$quantity</p>
-                                                    <button class='detail-btn'>View Details</button>  
-                                                    <button class='cancelorder'>Cancel Order</button>
-                                                </div>
-                                                <div class='deliver-details'>
-                                                    <div class='inlinedetails'>
-                                                        <div class='date'>
-                                                            <p class='label'>Delivered on $date</p>
-                                                        </div>
-                                                        <div class='deliverto'>
-                                                            <p class='label'>Ship to $name</p>    
-                                                        </div>
+                                    $quantity=$row['quantity'];
+                                    $price=$row['price'];
+                                    $itemname=$row2['name'];
+                                    $img1=$row2['img1'];
+                                    $address=$row['address'];
+                                    $contactnumber=$row['contactnumber'];
+                                    $name=$row['fullname'];
+                                    $date=$row['deliverydate'];
+                                    $status=$row['status'];
+                                    $orderid=$row['orderid'];
+                                    echo "<div class='order'>
+                                            <div class='orderimgdiv'>
+                                                <img src='./itemimages/$img1'  class='orderimg'>
+                                            </div>
+                                            <div class='orderdesc'>
+                                                <h2 class='ordername'>$itemname </h2>
+                                                <span class='cost'> ₹ $price</span>
+                                                <span class='quantity'><span class='label'>Quantity :</span>$quantity</span><br>
+                                                <div class='status'>$status</div>
+                                                <button class='detail-btn'><a href='itempage.php?itemname=$itemname'>View Details</a></button>";
+                                                if($status=='Order Placed')
+                                                {
+                                                    echo "<button class='cancelorder'><a href='../includes/deleteorderitem.php?orderid=$orderid'>Cancel Order</a></button>";
+                                                }
+                                             echo "       
+                                            </div>
+                                            <div class='deliver-details'>
+                                                <div class='inlinedetails'>
+                                                    <div class='date'>
+                                                        <p class='label'>Delivered on</p>
+                                                        $date
                                                     </div>
+                                                    <div class='deliverto'>
+                                                        <p class='label'>Ship to</p>
+                                                        $name  
+                                                    </div>
+                                                </div>
+                                                <div class='inlinedetails'>
                                                     <div class='address'>
-                                                        <p class='label'>Address  $address</p>  
+                                                        <p class='label'>Address</p>
+                                                        $address 
                                                     </div>
-                                                </div>
-                                            </div>";
+                                                    <div class='deliverto inline-data'>
+                                                        <p class='label'>Contact:&nbsp;</p>
+                                                        $contactnumber 
+                                                    </div>
+                                                </div>   
+                                            </div>
+                                        </div>";
                                         
-                                    }
-
                                 }
+
+                            }
                             
-                            }
-                            if ($flag==1)
-                            {
-                                $set="pay";
-                                echo "<div class=btn-div>
-                                <button class='proceed' name='proceedtopay-btn'><a href='payment.php?proceedtopay=$set'>Proceed to pay</a><i class='fas fa-coins'></i></button>
-                                </div>";
-                            }
-                            else
-                            {
-                                echo "<h2>No items order till date.</h2>";
-                                exit();
-                            }    
                         }
-                        
-                    } 
-                }
-                else
-                    {
-                        header("Location:./homepage.php?error=Admin doesnt have a cart");
-                        exit();
+                        if ($flag==0)
+                        {
+                            echo "<h2>No items ordered till date.</h2>";
+                            exit();
+                        }    
                     }
-                }
+                        
+                } 
                 else
                 {
-                    header("Location:./homepage.php?error=Sign in or Sign up");
+                    header("Location:./homepage.php?error=Admin doesnt have a cart");
                     exit();
                 }
+            }
+            else
+            {
+                header("Location:./homepage.php?error=Sign in or Sign up");
+                exit();
+            }
         ?>
     </div>
     <!-- Orders end -->
